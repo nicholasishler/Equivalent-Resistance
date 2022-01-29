0c5987378abd535885cab1731143a4829c37cdb5
nicholasishler <nicholas.ishler27@gmail.com>
2022-01-29 16:51:33 -0500

    Update eq_resistance.m

--- eq_resistance.m
+++ eq_resistance.m
@@ -11,16 +11,24 @@ desired_resistance = input('Enter the desired resistance in ohms: ');
 series_resistance = r1 + r2;
 
 if desired_resistance == r1
-    fprintf("To achieve %8.2f ohms, use the first resistor in series.\n", desired_resistance)
+    fprintf("To achieve %3.2f ohms, use the first resistor in series.\n", desired_resistance)
 elseif desired_resistance == r2
     fprintf("To achieve %3.2f ohms, use the second resistor in series.\n", desired_resistance)
 elseif desired_resistance == r3
-    fprintf("To achieve %8.2f ohms, use the third resistor in series.\n", desired_resistance)
+    fprintf("To achieve %3.2f ohms, use the third resistor in series.\n", desired_resistance)
 else
     eqr1 = 1/((1/(r2+r3))+(1/r1));
     eqr2 = 1/((1/(r1+r3))+(1/r2));
     eqr3 = 1/((1/(r2+r1))+(1/r3));
-
-    min_eqr_difference = min([abs(eqr1-desired_resistance) abs(eqr2-desired_resistance) abs(eqr3-desired_resistance)]);
-    fprintf("The closest resistance with these resistors is %f \n The order of resistance is %f and %f in series and those will be in parallel with the %f ohm resistor", )
+    values = [(eqr1-desired_resistance) abs(eqr2-desired_resistance) abs(eqr3-desired_resistance)];
+    min_eqr_difference = min([values]);
+    closest_res = desired_resistance + min_eqr_difference;
+    index = find(min_eqr_difference==values);
+    if index == 1
+        fprintf("The closest resistance with these resistors is %3.2f. \n The order of resistance is %3.2f ohm and %3.2f ohm resistors in series and those will be in parallel with the %3.2f ohm resistor.\n", closest_res, r2, r3, r1)
+    elseif index == 2
+        fprintf("The closest resistance with these resistors is %3.2f. \n The order of resistance is %3.2f ohm and %3.2f ohm resistors in series and those will be in parallel with the %3.2f ohm resistor.\n", closest_res, r1, r3, r2)
+    else
+        fprintf("The closest resistance with these resistors is %3.2f. \n The order of resistance is %3.2f ohm and %3.2f ohm resistors in series and those will be in parallel with the %3.2f ohm resistor.\n", closest_res, r2, r1, r3)
+    end
 end
